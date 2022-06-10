66fad64f592ebe2708073a5d5202e5b69e4eb5a6
Vishwajeetdeulkar <vishwajeetd13@gmail.com>
2022-06-10 09:26:21 +0530

    recursion folder added

---
+++ recursion/sortArray.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insert(vector<int>& v,int temp){
+	if(v.size()==0 || v[v.size()-1]<=temp){
+		v.push_back(temp);
+		return;
+	}
+	int val=v[v.size()-1];
+	v.pop_back();
+	insert(v,temp);
+	v.push_back(val);
+}
+void sort(vector<int>& v){
+	if(v.size()==1) return;
+	int temp=v[v.size()-1];
+	v.pop_back();
+	sort(v);
+	insert(v,temp);
+}
+int main(){
+	vector<int> nums{1,7,2,9,34,44,33,2,2,3,4};
+	sort(nums);
+	for(auto i:nums) cout<<i<<" ";
+	return 0;
+}
