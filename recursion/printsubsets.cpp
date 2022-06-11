b41b9b114ba0d613b1ece4e967710329d7d03f85
Vishwajeetdeulkar <vishwajeetd13@gmail.com>
2022-06-11 07:13:19 +0530

    power ser & kth symbol in ge=rammer program added

---
+++ recursion/printsubsets.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(string ip, string op){
+	if(ip.length()==0){
+		cout<<op<<"\n";
+		return;
+	}
+	string op1=op;
+	op.push_back(ip[0]);
+	string op2=op;
+	ip.erase(ip.begin()+0);
+	solve(ip,op1);
+	solve(ip,op2);
+	return;
+}
+int main(){
+	string ip="abc";
+	string op="";
+	solve(ip,op);
+	return 0;
+}
